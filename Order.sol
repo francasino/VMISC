3f2ce56d66a3fbd63e0f871fcab69b2e411166e3
Fran Casino <35731320+francasino@users.noreply.github.com>
2018-12-06 12:23:46 +0200

    Update Order.sol

--- Order.sol
+++ Order.sol
@@ -123,7 +123,7 @@ contract Order{
     // function to check the contents of the contract, the customer will check it and later will trigger if correct
     // only customer can check it 
     // customer will loop outside for this, getting the number of products before with getNumberOfProducts
-    function getOrder (uint _productId) returns (Product) {
+    function getProduct (uint _productId) returns (Product) {
     	require(msg.sender==customer);
     	require(_productId > 0 && _productId <= productsCount); 
 
